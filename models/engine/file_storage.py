af14caf2b6bd3f909e64cfce4c8a72ced60a217c
Humberto Tejada <humtej1204@gmail.com>
2022-03-05 14:21:30 -0500

    Correcting pycodetyle engine/**files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+'''File with the class FileStorage that serializes instances
+to a JSON file and deserializes JSON file to instances'''
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -8,21 +12,36 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-models = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
-            "Amenity": Amenity, "Place": Place, "Review": Review}
+
+models = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+        }
+
 
 class FileStorage:
+    '''FileStorage Class'''
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        '''Returns the dictionary __objects'''
         return (FileStorage.__objects)
 
     def new(self, obj):
+        '''Sets in __objects the obj with key
+        <obj class name>.id'''
         key = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
+        '''Serializes __objects to the JSON file
+        (path: __file_path)'''
         dic = {}
         for k, v in FileStorage.__objects.items():
             dic[k] = v.to_dict()
@@ -30,6 +49,9 @@ class FileStorage:
             json.dump(dic, f)
 
     def reload(self):
+        '''Deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists;
+        otherwise, do nothing.)'''
         try:
             with open(FileStorage.__file_path, "r", encoding="UTF-8") as f:
                 dict_dict = json.load(f)
