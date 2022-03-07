420ff7d6f40419cf0b3962a36d877c2a9567514c
Michael Chambilla <73969861+Mcfreestyle@users.noreply.github.com>
2022-03-06 19:14:43 -0500

    correcting line 22

--- models/base_model.py
+++ models/base_model.py
@@ -19,11 +19,11 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, frmat))
                 if key != "__class__":
                     setattr(self, key, value)
-            return
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         '''Method that represents the class objects as a string'''
